a80302aa4cd1c6edecfb9bba621e68879958d283
pschloss <pschloss@umich.edu>
2023-03-07 11:24:23 -0500

    Regenerate original figures

---
+++ workflow/scripts/plot_my_figure_4.R
@@ -0,0 +1,97 @@
+#!/usr/bin/env Rscript
+
+library(tidyverse)
+
+library(ggtext)
+
+add_nl <- function(x) {
+
+  paste0("N~L~ = ", format(as.numeric(x), big.mark = "\\\\,"))
+
+}
+
+pretty_distances <- c(bray = "Bray-Curtis",
+                      euclidean = "Euclidean",
+                      poisson = "Poisson",
+                      logFC = "Mean Squared Difference",
+                      uunifrac = "Unweighted UniFrac",
+                      wunifrac = "Weighted UniFrac")
+
+pretty_transform <- c(deseq = "DeSeq VS",
+                    none = "Raw counts",
+                    proportion = "Relative abundance",
+                    subsample15 = "Sub-sampled (15%)",
+                    upperquartile = "Upper Quartile Log Fold Change",
+                    rarefaction00 = "Rarefaction")
+
+cluster_method <- "pam"
+
+read_tsv("data/simulation_clusters.tsv.gz") %>%
+  rename(subset = fracCorrectPred, all = fracCorrect) %>%
+  filter(filter == "filter" &
+          transform %in% names(pretty_transform) &
+          distance %in% names(pretty_distances)) %>%
+  filter(method == cluster_method) %>%
+  filter(fraction != "s1") %>%
+  mutate(fraction = as.numeric(fraction)) %>%
+  filter((distance == "bray" &
+            transform %in% c("proportion", "subsample15", "none", "deseq")) |
+        (distance == "euclidean" &
+            transform %in% c("none", "deseq", "subsample15")) |
+        (distance == "poisson" &
+            transform %in% c("subsample15", "none")) |
+        (distance == "logFC" &
+            transform %in% c("upperquartile", "subsample15")) |
+        (distance == "uunifrac" &
+            transform %in% c("subsample15", "proportion")) |
+        (distance == "wunifrac" &
+            transform %in% c("subsample15", "none", "proportion", "deseq"))
+        ) %>%
+  group_by(fraction, n_seqs, transform, distance) %>%
+  summarize(mean = mean(all, na.rm = TRUE),
+            lci = quantile(all, 0.025, na.rm = TRUE),
+            uci = quantile(all, 0.975, na.rm = TRUE), .groups = "drop") %>%
+  mutate(distance = factor(pretty_distances[distance],
+                          levels = pretty_distances),
+        transform = factor(pretty_transform[transform],
+                            levels = pretty_transform)) %>%
+  ggplot(aes(x = fraction, y = mean, group = transform,
+            color = transform, shape = transform, fill = transform)) +
+  geom_hline(yintercept = 0.5, color = "darkgray") +
+  geom_line(position = position_dodge(width = 0.075)) +
+  geom_linerange(aes(ymin = lci, ymax = uci),
+                alpha = 0.6, position = position_dodge(width = 0.075),
+                show.legend = FALSE) +
+  geom_point(position = position_dodge(width = 0.075),
+            size = 2) +
+  facet_grid(n_seqs ~ distance,
+            labeller = labeller(n_seqs = add_nl)) +
+  scale_y_continuous(expand = c(0, 0)) +
+  scale_fill_manual(
+    values = c("#1b9e77", "#d95f02", "#7570b3", "#e7298a", "#66a61e", "#000000")
+  ) +
+  scale_color_manual(
+    values = c("#1b9e77", "#d95f02", "#7570b3", "#e7298a", "#66a61e", "#000000")
+  ) +
+  scale_shape_manual(
+    values = c(21, 22, 23, 24, 25, 21)
+  ) +
+  coord_cartesian(ylim = c(0.4, 1.05)) +
+  labs(x = "Effect Size",
+      y = "Accuracy",
+      color = "Normalization Method:",
+      fill = "Normalization Method:",
+      shape = "Normalization Method:") +
+  theme_light() +
+  theme(
+    legend.position = "top",
+    legend.box = "horizontal",
+    legend.margin = margin(t = 0, r = -0.5, b = 0, l = -0.5, unit = "cm"),
+    strip.text.y = element_markdown()
+  ) +
+  guides(color = guide_legend(nrow = 1),
+        fill = guide_legend(nrow = 1),
+        shape = guide_legend(nrow = 1))
+
+ggsave(paste0("results/figures/my_figure_4.pdf"),
+        width = 11, height = 10)
---
+++ workflow/scripts/plot_my_figure_5.R
@@ -0,0 +1,80 @@
+#!/usr/bin/env Rscript
+
+library(tidyverse)
+library(ggtext)
+
+add_es <- function(x) {
+
+  paste0("ES = ", format(as.numeric(x), nsmall = 2L))
+
+}
+
+add_nl <- function(x) {
+
+  paste0("N~L~ = ", format(as.numeric(x), big.mark = "\\,"))
+
+}
+
+pretty_distances <- c(bray = "Bray-Curtis",
+                      euclidean = "Euclidean",
+                      poisson = "Poisson",
+                      logFC = "Mean Squared Difference",
+                      uunifrac = "Unweighted UniFrac",
+                      wunifrac = "Weighted UniFrac")
+
+clusters <- read_tsv("data/simulation_clusters.tsv.gz") %>%
+  filter(filter == "filter") %>%
+  filter(method == "pam") %>%
+  filter(distance != "bcv") %>%
+  filter(str_detect(transform, "subsample")) %>%
+  filter(fraction != "s1" & fraction != "1") %>%
+  select(fraction, n_seqs, rep, transform, distance, method, fracCorrect) %>%
+  group_by(fraction, n_seqs, transform, distance, method) %>%
+  summarize(median = median(fracCorrect),
+            lci = quantile(fracCorrect, 0.025),
+            uci = quantile(fracCorrect, 0.975), .groups = "drop") %>%
+  mutate(quant = as.numeric(str_replace(transform, "subsample", "")),
+        distance = pretty_distances[distance])
+
+clusters %>%
+  ggplot(aes(x = quant, y = median, group = distance,
+            color = distance, fill = distance, shape = distance)) +
+  geom_segment(x = 0, y = 1,
+              xend = 100, yend = 0, color = "gray") +
+  geom_linerange(aes(ymin = lci, ymax = uci),
+                alpha = 0.6, position = position_dodge(width = 1.5),
+                show.legend = FALSE) +
+  geom_line(position = position_dodge(width = 1.5)) +
+  geom_point(position = position_dodge(width = 1.5),
+            size = 2) +
+  facet_grid(n_seqs ~ fraction,
+            labeller = labeller(fraction = add_es, n_seqs = add_nl)) +
+  scale_fill_manual(
+    values = c("#1b9e77", "#d95f02", "#7570b3", "#e7298a", "#66a61e", "#000000")
+  ) +
+  scale_color_manual(
+    values = c("#1b9e77", "#d95f02", "#7570b3", "#e7298a", "#66a61e", "#000000")
+  ) +
+  scale_shape_manual(
+    values = c(21, 22, 23, 24, 25, 21)
+  ) +
+  coord_cartesian(ylim = c(0.4, 1.05),
+                  xlim = c(0, 40)) +
+  labs(x = "Library Size Minimum Quantile (%)",
+      y = "Accuracy",
+      color = "Distance Calculation:",
+      fill = "Distance Calculation:",
+      shape = "Distance Calculation:") +
+  theme_light() +
+  theme(
+    legend.position = "top",
+    legend.box = "horizontal",
+    legend.margin = margin(t = 0, r = -0.5, b = 0, l = -0.5, unit = "cm"),
+    strip.text.y = element_markdown()
+  ) +
+  guides(color = guide_legend(nrow = 1),
+        fill = guide_legend(nrow = 1),
+        shape = guide_legend(nrow = 1))
+
+ggsave("results/figures/my_figure_5.pdf",
+        width = 11, height = 10)
